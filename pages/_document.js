809c32339e57bcc1593145d2ddd30ee399d7d9b3
Afnan shawawreh <64199018+AfnanShawawreh@users.noreply.github.com>
2020-08-26 16:00:48 +0300

    update my app

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,41 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from "next/document";
 
-const APP_NAME = 'wodworx'
-const APP_DESCRIPTION = 'This is an example of using next-pwa plugin'
+const APP_NAME = "wodworx";
+const APP_DESCRIPTION = "Login page ";
 
 export default class extends Document {
   static async getInitialProps(ctx) {
-    return await Document.getInitialProps(ctx)
+    return await Document.getInitialProps(ctx);
   }
 
   render() {
     return (
-      <html lang='en' dir='ltr'>
+      <html lang="en" dir="ltr">
         <Head>
-          <meta name='application-name' content={APP_NAME} />
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content={APP_NAME} />
-          <meta name='description' content={APP_DESCRIPTION} />
-          <meta name='format-detection' content='telephone=no' />
-          <meta name='mobile-web-app-capable' content='yes' />
-          <meta name='theme-color' content='#FFFFFF' />
-          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
-          
-          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
-          <link rel='manifest' href='/manifest.json' />
-          <link rel='shortcut icon' href='/icons/favicon.ico' />
-          <style>{
-            `
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#FFFFFF" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+          <style>{`
             html, body, #__next {
               height: 100%;
             }
@@ -36,14 +45,13 @@ export default class extends Document {
             h1 {
               text-align: center;
             }
-            `
-          }</style>
+            `}</style>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
