9374aa38d988031adb46beb734144b6ed494aa63
Afnan shawawreh <64199018+AfnanShawawreh@users.noreply.github.com>
2020-08-27 22:36:47 +0300

    last12

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
 
 const Nav = () => (
-  <div>
-    <img src="/static/logo.png" alt="logo" height="50" />
-    <h1>WODWORX</h1>
+  <div class="flex items-center  justify-start">
+ 
+    <img   class=" inline-block h-12 w-12 " viewBox="0 0 24 24"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"src="/static/logo.png"  alt="logo " 
+   
+     />
+
+    <h1 class=" text-white text-xs text-left"	> WODWORX </h1>
+    
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,17 @@
 import React from "react";
 import axios from "axios";
 import cookie from "js-cookie";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
+
 import Layout from "../components/Layout";
-import { StylesProvider } from "@material-ui/core/styles";
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
     </Typography>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
 export default function SignIn() {
-  const classes = useStyles();
-
   const [values, setValues] = React.useState({
     Email: "",
     password: "",
@@ -81,21 +41,18 @@ export default function SignIn() {
       )
       .then((res) => {
         let resData = JSON.parse(res.request.response);
-        if (resData.data.token) {
-          document.getElementById("result").innerText = "Login Successed! ";
-          alert("Login Successed!");
+        if (resData.data) {
+          console.log(resData.data.token);
+          alert( 'SUCCESSFUL LOGIN' );
         }
         if (resData.data.errors.email) {
-          document.getElementById(
-            "result"
-          ).innerText = resData.data.errors.email.join("\n");
+          console.log(resData.data.errors.email);
+
           alert(resData.data.errors.email.join("\n"));
         } else {
           if (resData.data.errors.password) {
-            document.getElementById(
-              "result"
-            ).innerText = resData.data.errors.password.join("\n");
-            alert(resData.data.errors.password.join("\n"));
+            alert(resData.data.errors.password);
+            console.log(resData.data.errors.password.join("\n"));
           }
         }
       })
@@ -106,79 +63,86 @@ export default function SignIn() {
   };
 
   return (
-    <StylesProvider injectFirst>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
+    <div class=" .bg-gry-500 " style={{ backgroundColor: "#2d2d2d" }}>
+      <div>
         <Layout />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <img src="/static/logo.png" alt="logo" height="50" />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
+      </div>
+      <div class="  min-h-screen  flex items-start  justify-center ">
+        <div>
           <form
-            className={classes.form}
+            class=".bg-gry-100 shadow-md rounded px-6 pt-6 pb-8 mb-4 "
+            frameBorder={0}
+            style={{ backgroundColor: "#3f3f3f" }}
+            aria-hidden="false"
+            tabIndex={0}
             onSubmit={onSubmit}
             method="post"
             noValidate
           >
-            <TextField
-              value={values.Email}
-              onChange={handleChange("Email")}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              value={values.password}
-              onChange={handleChange("password")}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              id="password"
-              type="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs></Grid>
-              <Grid item>
-                <Link href="signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-              <Grid item>
-                <p id="result"></p>
-              </Grid>
-            </Grid>
+            <div class="mb-4">
+              <h1 class="text-gray-400 text-xl font-bold  px-8 pt-2 pb-2 italic">
+                Login to your account
+              </h1>
+              <input
+                class=" .bg-gry-300 shadow  appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
+                value={values.Email}
+                onChange={handleChange("Email")}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                placeholder="Your email address"
+                autoFocus
+              />
+            </div>
+            <div class="mb-6">
+              <input
+                class=" bg-gry-300 shadow appearance-none border   rounded w-full py-2 px-3 text-gray-500 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Your password"
+                value={values.password}
+                onChange={handleChange("password")}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                autoComplete="current-password"
+              />
+            </div>
+            <div>
+              <button
+                class="bg-blue-500  w-full py-2 px-3 hover:bg-blue-700 text-white font-bold  rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+                variant="contained"
+              >
+                Sign In
+              </button>
+
+              <div>
+                <a
+                  class="inline-block align-baseline  py-2 px-3 font-bold text-sm text-blue-500 hover:text-blue-800"
+                  href="#"
+                >
+                  Forgot Password?
+                </a>
+                <p class=" text-gray-400 text-xs py-2 px-3 italic">
+                  Need an account? Absolutely! Sign Up for free.
+                </p>
+              </div>
+            </div>
           </form>
         </div>
-        <Box mt={8}>
-          <Copyright />
-        </Box>
-      </Container>
-    </StylesProvider>
+      </div>
+    </div>
   );
 }
+{
+}
+
